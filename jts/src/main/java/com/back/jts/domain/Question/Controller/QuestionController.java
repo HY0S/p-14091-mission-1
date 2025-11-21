7971bee5089e4b4880f24470efaaaae09eee597c
김효신 <kimhyoshin67@gachon.ac.kr>
2025-11-21 14:24:17 +0900

    2-11 완료

--- jts/src/main/java/com/back/jts/domain/Question/Controller/QuestionController.java
+++ jts/src/main/java/com/back/jts/domain/Question/Controller/QuestionController.java
@@ -6,20 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         model.addAttribute("questionList", questionService.getList());
         return "questionlist";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Long id) {
         model.addAttribute("question", questionService.getQuestion(id));
         return "question_detail";
