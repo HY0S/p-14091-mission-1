286c7e1750b8bcb645e62df84d184874e5267404
김효신 <kimhyoshin67@gachon.ac.kr>
2025-11-21 14:04:20 +0900

    2-08

---
+++ jts/src/main/java/com/back/jts/domain/Question/Controller/QuestionController.java
@@ -0,0 +1,47 @@
+package com.back.jts.domain.Question.Controller;
+
+import com.back.jts.domain.Question.Entity.Question;
+import com.back.jts.domain.Question.Repository.QuestionRepository;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Controller
+public class QuestionController {
+    private final QuestionRepository questionRepository;
+
+    public QuestionController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @GetMapping("/question/list")
+    @ResponseBody
+    public String list() throws IOException {
+        List<Question> questions = questionRepository.findAll();
+        
+        StringBuilder tableRows = new StringBuilder();
+        for (Question question : questions) {
+            String subject = question.getSubject() != null ? question.getSubject() : "제목 없음";
+            String createdDate = question.getCreatedDate() != null 
+                ? question.getCreatedDate().toString() 
+                : "";
+            
+            tableRows.append(String.format(
+                "    <tbody>\n    <td>%d</td>\n    <th>%s</th>\n    <th>%s</th>\n    </tbody>\n",
+                question.getId(),
+                subject,
+                createdDate
+            ));
+        }
+        
+        ClassPathResource resource = new ClassPathResource("templates/questionlist.html");
+        String htmlTemplate = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        
+        return htmlTemplate.replace("{{QUESTION_ROWS}}", tableRows.toString());
+    }
+}
